14c47587aedf39798d76d5cc29654ca22ca432a1
mohammadsh97 <mohammadsh.97@hotmail.com>
2022-11-20 03:19:22 +0200

    Distribute the services(Business Layer) and controller(Request Handler Layer) appropriately

--- src/main/java/com/cryptographyServer/cryptography/server/controller/KeyController.java
+++ src/main/java/com/cryptographyServer/cryptography/server/controller/KeyController.java
@@ -2,27 +2,18 @@ package com.cryptographyServer.cryptography.server.controller;
 
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
-import java.util.*;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
-import com.cryptographyServer.cryptography.server.entity.KeyEntity;
 import com.cryptographyServer.cryptography.server.model.Crypto;
 import com.cryptographyServer.cryptography.server.model.Decrypt;
 import com.cryptographyServer.cryptography.server.model.Verify;
-import com.cryptographyServer.cryptography.server.repository.KeyRepository;
 import org.json.*;
 import com.cryptographyServer.cryptography.server.services.KeyService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-
-@Service
 @RestController
 @RequestMapping(path = "/api")
 public class KeyController {
@@ -30,134 +21,34 @@ public class KeyController {
     @Autowired
     private KeyService keyService;
 
-    @Autowired
-    private KeyRepository keyRepository;
-
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    public Iterable<KeyEntity> getListOfKeyEntity() {
-        return keyRepository.findAll();
-    }
-
-    public KeyEntity getListOfKeyEntityByUniqueID(String uniqueID) {
-        return keyRepository.findByUniqueID(uniqueID);
-    }
 
     @GetMapping(path = "/")
     public String getAllKeys() throws JSONException {
-        StringBuilder result = new StringBuilder("KeyId, publicKey \n");
-//        for (Map.Entry<String, KeyPair> m : keyService.getMap().entrySet()) {
-//            result.append(m.getKey()).append(", ").
-//                    append(keyService.encodeKey(m.getValue().getPublic())).append(" \n");
-//        }
-
-        Iterator itr = getListOfKeyEntity().iterator();
-
-        while (itr.hasNext()) {
-            KeyEntity element = (KeyEntity) itr.next();
-            result.append(element.getUniqueID()).append(", ").
-                    append(element.getPublicKey()).append(" \n");
-        }
-
-        JSONArray toJSONArray = CDL.toJSONArray(result.toString());
-        logger.debug("View all keys generated....");
-        return String.valueOf(toJSONArray);
+        return this.keyService.getKeyService();
     }
 
-    @PostMapping("/generate")
+    @GetMapping("/generate")
     public String generateKeys() throws JSONException {
-        // Initialization of key pair for encryption and decryption.
-        KeyPair keyPair = keyService.getKeyPair();
-
-        logger.debug("generate pair key....");
-
-        KeyService.uniqueID = UUID.randomUUID().toString();
-//        keyService.addKey(KeyService.uniqueID, keyPair);
-        keyRepository.save(
-                new KeyEntity(KeyService.uniqueID,
-                        keyService.encodeKey(keyPair.getPrivate()),
-                        keyService.encodeKey(keyPair.getPublic())));
-
-        logger.debug("saved key in DB....");
-
-        JSONObject result = new JSONObject();
-        result.put("keyId", KeyService.uniqueID);
-
-        return String.valueOf(result);
+        return keyService.generateKeyService();
     }
 
     @PostMapping("/encrypt")
-    public String encrypt(@RequestBody Crypto jsonData) throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException {
-//        String keyId = jsonData.get("keyId"), data = jsonData.get("data");
-        String keyId = jsonData.getKeyId();
-        String data = jsonData.getData();
-        // Save the data
-        KeyService.data = data;
-
-        // Encrypt plain as a cipher.
-        String encryptData = keyService.encrypt(getListOfKeyEntityByUniqueID(keyId), data);
-
-        JSONObject result = new JSONObject();
-        result.put("EncryptData", encryptData);
-
-        logger.debug("encrypted the data....");
-
-        return String.valueOf(result);
+    public String encrypt(@RequestBody Crypto jsonData) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeySpecException, InvalidKeyException {
+        return keyService.encrypt(jsonData);
     }
 
     @PostMapping(value = "/decrypt")
     public String decrypt(@RequestBody Decrypt jsonData) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, JSONException, InvalidKeySpecException {
-
-//        String keyId = jsonData.get("keyId"), encryptedData = jsonData.get("encryptedData");
-        String keyId = jsonData.getKeyId();
-        String encryptedData = jsonData.getEncryptedData();
-
-        // replaces all occurrences of " " to "+"
-        encryptedData = encryptedData.replace(" ", "+");
-
-        // Decrypt cipher to original plain.
-        String decryptResult = keyService.decrypt(encryptedData, getListOfKeyEntityByUniqueID(keyId));
-
-        JSONObject result = new JSONObject();
-        result.put("DecryptData", decryptResult);
-
-        logger.debug("decrypted the encrypted data....");
-
-        return String.valueOf(result);
+        return keyService.decrypt(jsonData);
     }
 
     @PostMapping("/sign")
     public String sign(@RequestBody Crypto jsonData) throws NoSuchAlgorithmException, SignatureException, InvalidKeyException, InvalidKeySpecException {
-
-//        String keyId = jsonData.get("keyId"), data = jsonData.get("data");
-        String keyId = jsonData.getKeyId();
-        String data = jsonData.getData();
-
-        Signature privateSignature = Signature.getInstance("SHA256withRSA");
-        privateSignature.initSign(keyService.decodePrivateKey(getListOfKeyEntityByUniqueID(keyId).getPrivateKey()));
-        privateSignature.update(data.getBytes(UTF_8));
-        byte[] signature = privateSignature.sign();
-
-        JSONObject result = new JSONObject();
-        result.put("sign", Base64.getEncoder().encodeToString(signature));
-
-        logger.debug("sign is success....");
-
-        return String.valueOf(result);
+        return keyService.sign(jsonData);
     }
 
     @PostMapping("/verify")
     public boolean verify(@RequestBody Verify jsonData) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, InvalidKeySpecException {
-
-        String keyId = jsonData.getKeyId(), data = jsonData.getData(), signature = jsonData.getSignature();
-        Signature publicSignature = Signature.getInstance("SHA256withRSA");
-
-        publicSignature.initVerify(keyService.decodePublicKey(getListOfKeyEntityByUniqueID(keyId).getPublicKey()));
-        publicSignature.update(data.getBytes(UTF_8));
-        byte[] signatureBytes = Base64.getDecoder().decode(signature);
-
-        logger.debug("verify is success....");
-
-        return publicSignature.verify(signatureBytes);
+        return keyService.verify(jsonData);
     }
 }
--- src/main/java/com/cryptographyServer/cryptography/server/services/KeyService.java
+++ src/main/java/com/cryptographyServer/cryptography/server/services/KeyService.java
@@ -5,30 +5,92 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
+import java.util.UUID;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.annotation.PostConstruct;
 
+import com.cryptographyServer.cryptography.server.model.Crypto;
+import com.cryptographyServer.cryptography.server.model.Decrypt;
+import com.cryptographyServer.cryptography.server.model.Verify;
+import org.json.JSONObject;
+import org.slf4j.Logger;
 import com.cryptographyServer.cryptography.server.entity.KeyEntity;
-import org.springframework.stereotype.Component;
+import com.cryptographyServer.cryptography.server.repository.KeyRepository;
+import org.json.CDL;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-@Component
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+@Service
 public class KeyService {
-    public static String uniqueID;
 
-    public static String data;
+    @Autowired
+    private KeyRepository keyRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public String uniqueID;
+
+    public String data;
 
     private static KeyPairGenerator keyPairGenerator = null;
 
     private final SecureRandom random = new SecureRandom();
 
+    private Iterable<KeyEntity> getListOfKeyEntity() {
+        return keyRepository.findAll();
+    }
+
+    public KeyEntity getListOfKeyEntityByUniqueID(String uniqueID) {
+        return keyRepository.findByUniqueID(uniqueID);
+    }
+
     public KeyPair getKeyPair() {
         return keyPairGenerator.generateKeyPair();
     }
 
-    public String encrypt(KeyEntity keyEntity, String data) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidKeySpecException {
+    public String getKeyService() throws JSONException {
+        StringBuilder result = new StringBuilder("KeyId, publicKey \n");
+
+        for (KeyEntity element : getListOfKeyEntity()) {
+            result.append(element.getUniqueID()).append(", ").
+                    append(element.getPublicKey()).append(" \n");
+        }
+
+        JSONArray toJSONArray = CDL.toJSONArray(result.toString());
+        logger.debug("View all keys generated....");
+        return String.valueOf(toJSONArray);
+    }
+
+    public String generateKeyService() throws JSONException {
+        // Initialization of key pair for encryption and decryption.
+        KeyPair keyPair = getKeyPair();
+
+        logger.debug("generate pair key....");
+
+        uniqueID = UUID.randomUUID().toString();
+        keyRepository.save(
+                new KeyEntity(uniqueID,
+                        encodeKey(keyPair.getPrivate()),
+                        encodeKey(keyPair.getPublic())));
+
+
+        logger.debug("saved key in DB....");
+
+        JSONObject result = new JSONObject();
+        result.put("keyId", uniqueID);
+
+        return String.valueOf(result);
+    }
+
+    public String encryptHelper(KeyEntity keyEntity, String data) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidKeySpecException {
 
         Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 
@@ -38,7 +100,24 @@ public class KeyService {
         return Base64.getEncoder().encodeToString(cipherContent);
     }
 
-    public String decrypt(String encryptedData, KeyEntity keyEntity) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidKeySpecException {
+    public String encrypt(Crypto jsonData) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeySpecException, InvalidKeyException {
+        String keyId = jsonData.getKeyId();
+        String data = jsonData.getData();
+        // Save the data
+        this.data = data;
+
+        // Encrypt plain as a cipher.
+        String encryptData = encryptHelper(getListOfKeyEntityByUniqueID(keyId), data);
+
+        JSONObject result = new JSONObject();
+        result.put("EncryptData", encryptData);
+
+        logger.debug("encrypted the data....");
+
+        return String.valueOf(result);
+    }
+
+    public String decryptHelper(String encryptedData, KeyEntity keyEntity) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidKeySpecException {
 
         Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
         cipher.init(Cipher.DECRYPT_MODE, decodePrivateKey(keyEntity.getPrivateKey()));
@@ -46,6 +125,55 @@ public class KeyService {
         byte[] decryptedContent = cipher.doFinal(cipherContentBytes);
 
         return new String(decryptedContent);
+    }
+
+    public String decrypt(Decrypt jsonData) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, JSONException, InvalidKeySpecException {
+        String keyId = jsonData.getKeyId();
+        String encryptedData = jsonData.getEncryptedData();
+
+        // replaces all occurrences of " " to "+"
+        encryptedData = encryptedData.replace(" ", "+");
+
+        // Decrypt cipher to original plain.
+        String decryptResult = decryptHelper(encryptedData, getListOfKeyEntityByUniqueID(keyId));
+
+        JSONObject result = new JSONObject();
+        result.put("DecryptData", decryptResult);
+
+        logger.debug("decrypted the encrypted data....");
+
+        return String.valueOf(result);
+    }
+
+    public String sign(Crypto jsonData) throws NoSuchAlgorithmException, SignatureException, InvalidKeyException, InvalidKeySpecException {
+        String keyId = jsonData.getKeyId();
+        String data = jsonData.getData();
+
+        Signature privateSignature = Signature.getInstance("SHA256withRSA");
+        privateSignature.initSign(decodePrivateKey(getListOfKeyEntityByUniqueID(keyId).getPrivateKey()));
+        privateSignature.update(data.getBytes(UTF_8));
+        byte[] signature = privateSignature.sign();
+
+        JSONObject result = new JSONObject();
+        result.put("sign", Base64.getEncoder().encodeToString(signature));
+
+        logger.debug("sign is success....");
+
+        return String.valueOf(result);
+    }
+
+    public boolean verify(Verify jsonData) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, InvalidKeySpecException {
+
+        String keyId = jsonData.getKeyId(), data = jsonData.getData(), signature = jsonData.getSignature();
+        Signature publicSignature = Signature.getInstance("SHA256withRSA");
+
+        publicSignature.initVerify(decodePublicKey(getListOfKeyEntityByUniqueID(keyId).getPublicKey()));
+        publicSignature.update(data.getBytes(UTF_8));
+        byte[] signatureBytes = Base64.getDecoder().decode(signature);
+
+        logger.debug("verify is success....");
+
+        return publicSignature.verify(signatureBytes);
     }
 
     public String encodeKey(Key key) {
@@ -57,16 +185,14 @@ public class KeyService {
         byte[] keyBytes = Base64.getDecoder().decode(keyStr);
         X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        PublicKey key = keyFactory.generatePublic(spec);
-        return key;
+        return keyFactory.generatePublic(spec);
     }
 
     public PrivateKey decodePrivateKey(String keyStr) throws NoSuchAlgorithmException, InvalidKeySpecException {
         byte[] keyBytes = Base64.getDecoder().decode(keyStr);
         PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        PrivateKey key = keyFactory.generatePrivate(keySpec);
-        return key;
+        return keyFactory.generatePrivate(keySpec);
     }
 
     @PostConstruct
