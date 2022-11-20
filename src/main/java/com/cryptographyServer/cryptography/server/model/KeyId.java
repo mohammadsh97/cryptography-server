146fab8bd393fa49c629b0df9a2e7c2c25480c00
mohammadsh97 <mohammadsh.97@hotmail.com>
2022-11-20 03:04:46 +0200

    Add "KeyId" class to remove the duplicate code

---
+++ src/main/java/com/cryptographyServer/cryptography/server/model/KeyId.java
@@ -0,0 +1,13 @@
+package com.cryptographyServer.cryptography.server.model;
+
+public class KeyId {
+    private String keyId;
+
+    public String getKeyId() {
+        return keyId;
+    }
+
+    public void setKeyId(String keyId) {
+        this.keyId = keyId;
+    }
+}
